ead24298a575ce966ed8155cca77a9e332379a82
amir <mr.amirentezari@gmail.com>
2024-03-08 21:30:03 +0330

    refactor(utils): get index of model as arg in plot_membrane_potential

--- code/plots.py
+++ code/plots.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 from pymonntorch import NeuronGroup
 
 
-def plot_membrane_potential(ng: NeuronGroup, ng_rec_name: str, title: str):
+def plot_membrane_potential(ng: NeuronGroup, ng_rec_name: str, title: str, model_idx: int = 3):
     net = ng.network
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
 
     ax1.plot(net[ng_rec_name, 0].variables["u"][:, :3], label='potential')
-    ax1.axhline(y=ng.behavior[3].init_kwargs['threshold'], color='r', linestyle='--', label='Threshold')
-    ax1.axhline(y=ng.behavior[3].init_kwargs['u_reset'], color='black', linestyle='--', label='u_reset')
+    ax1.axhline(y=ng.behavior[model_idx].init_kwargs['threshold'], color='r', linestyle='--', label='Threshold')
+    ax1.axhline(y=ng.behavior[model_idx].init_kwargs['u_reset'], color='black', linestyle='--', label='u_reset')
     ax1.set_xlabel('Time')
     ax1.set_ylabel('u(t)')
     ax1.set_title(f'Membrane Potential')
