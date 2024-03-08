380cfb85a8ef89757a6cc70acf7cec53aa6e47bb
amir <mr.amirentezari@gmail.com>
2024-03-08 11:40:39 +0330

    feat(model): define different current function including step function, sin function, ramp functio and log function.

--- code/currents.py
+++ code/currents.py
@@ -1,3 +1,6 @@
+import math
+
+import torch
 from pymonntorch import *
 
 
@@ -5,3 +8,59 @@ class ConstantCurrent(Behavior):
     def initialize(self, ng):
         self.value = self.parameter("value", None, required=True)
         ng.I = ng.vector(self.value)
+
+
+class StepFunctionCurrent(Behavior):
+    def initialize(self, ng):
+        self.value = self.parameter("value")
+        self.t0 = self.parameter("t0")
+        ng.I = ng.vector()
+
+    def forward(self, ng):
+        if ng.network.iteration * ng.network.dt >= self.t0:
+            ng.I = ng.vector(mode=self.value)
+
+
+class SinCurrent(Behavior):
+    def initialize(self, ng):
+        self.amplitude = self.parameter("amplitude", None, required=True)
+        self.frequency = self.parameter("frequency", None, required=True)
+        self.phase = self.parameter("phase", 0.0)
+        self.offset = self.parameter("offset", 0.0)
+
+        t = ng.network.iteration * ng.network.dt
+        ng.I = ng.vector()
+
+    def forward(self, ng):
+        t = ng.network.iteration * ng.network.dt
+        ng.I = torch.sin(ng.vector(self.frequency * t) + self.phase) * self.amplitude + self.offset
+
+
+class RampCurrent(Behavior):
+    def initialize(self, ng):
+        self.slope = self.parameter("slope", None, required=True)
+        ng.I = ng.vector()
+
+    def forward(self, ng):
+        t = ng.network.dt
+        ng.I += self.slope * t
+
+
+# class ExpDecayCurrent(Behavior):
+#     def initialize(self, ng):
+#         self.base = self.parameter("base")
+#         ng.I = ng.vector()
+#
+#     def forward(self, ng):
+#         t = ng.network.iteration * ng.network.dt
+#         ng.I = self.base + torch.exp(ng.vector(t))
+
+class LogCurrent(Behavior):
+    def initialize(self, ng):
+        self.horizontal_shift = self.parameter("horizontal_shift", 0.0)
+        self.vertical_shift = self.parameter("vertical_shift", 0.0)
+        ng.I = ng.vector()
+
+    def forward(self, ng):
+        t = ng.network.iteration * ng.network.dt
+        ng.I = self.vertical_shift + torch.log(ng.vector(t + self.horizontal_shift ))
